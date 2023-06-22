1ad18ba8042248cd40772b27e1435848c2dc7b3f
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-06-21 22:01:15 -0400

    updated README

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 -In this assignment I created a random password generator using javascript. This was a great way to learn more about the basics of javascript such as using variables, functions, arrays, for loops, and if statements. You can check out the finished product below.
 
-/*SCREENSHOT HERE*/
+![Alt text](<Screenshot (6).png>)
 
 URL:
 
+https://rcurcurato.github.io/Random-Password-Generator/
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Assignment Code*/
- 
+/*character arrays*/
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "=", "[", "]", "{", "}", ";", ":", "'", "<", ">", ",", ".", "?", "/"]
 
-var characterLength = "";
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
@@ -13,22 +12,24 @@ function generatePassword() {
   var characterLength = prompt("how many characters would you like your password to be? Must be between 8-128 characters.");
   /*sets character limit for password*/
   if (characterLength < 8 || characterLength > 128) {
+    /*brings user back to enter character length*/
     alert("password must contain between 8-128 characters");
     return
   }
-  /*asks user what characters to use in password*/
+  /*question prompts for password*/
   var hasUpperCase = confirm("Would you like to use uppercase letters in your password?");
   var hasLowerCase = confirm("Would you like to use lowercase letters in your password?");
   var hasNumbers = confirm("Would you like to use numbers in your password?");
   var hasSymbols = confirm("Would you like to use symbols in your password?");
   /*stores random characters for password*/
 
+  /*array that stores choices*/
   var answers = []
-  
 
+  /*if statements for user choices*/
   if (hasUpperCase) {
     answers = answers.concat(upperCase)
-   }
+  }
 
   if (hasLowerCase) {
     answers = answers.contcat(lowerCase)
@@ -41,24 +42,20 @@ function generatePassword() {
   if (hasSymbols) {
     answers = answers.concat(symbols)
   }
-  }
-  
-  var generatedPassword
-  
-  for (let i = 0; i < characterLength; ++i) {
-  
-    var random = Math.floor(Math.random() * answers.length);
-    generatedPassword = answers[random];
-  }
+}
 
+/*for loop for character amount*/
+for (let i = 0; i < answers; ++i) {
+  /*randomizes characters*/
+  var answers = Math.floor(Math.random() * generatedPassword.length);
+}
 
-
-  function writePassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- 
+
   passwordText.value = password;
-   
+
 }
 
 generateBtn.addEventListener("click", writePassword);
